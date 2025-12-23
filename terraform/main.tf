a960809bd9cb2a318fbb8862d9d606e8c29801bf
Luckuah <lululorang@gmail.com>
2025-12-23 18:31:01 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,22 @@ provider "aws" {
   region = var.region
 }
 
+module "vpc_G2MG03" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "vpc-steam-project"
+  cidr = "10.0.0.0/16"
+
+  # On crée des sous-réseaux dans 2 zones pour la haute disponibilité
+  azs             = ["eu-west-3a", "eu-west-3b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"] # Pour MongoDB (privé)
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"] # Pour l'accès Internet
+
+  enable_nat_gateway = true # Nécessaire pour que Mongo puisse télécharger des mises à jour
+  single_nat_gateway = true
+}
+
 # --------------------------
 # S3 Bucket Module
 # --------------------------
@@ -40,15 +56,18 @@ module "ecr_G2MG03" {
 # ECS Cluster Module
 # --------------------------
 module "ecs_G2MG03" {
-  source       = "./modules/ecs"
-  cluster_name = var.ecs_cluster_name
+  source             = "./modules/ecs"
+  cluster_name       = var.ecs_cluster_name
+  vpc_id             = module.vpc_G2MG03.vpc_id
+  subnet_ids         = module.vpc_G2MG03.private_subnets
+  security_group_ids = [var.ecs_sg_id]
 }
 
-# --------------------------
-# App Runner Service Module (optionnel)
-# --------------------------
-# module "apprunner_G2MG03" {
-#   source       = "./modules/apprunner"
-#   service_name = "apprunner-g2-mg03"
-#   ecr_repo_url = module.ecr_G2MG03.repository_url
-# }
+
+module "apprunner_G2MG03" {
+   source       = "./modules/apprunner"
+   service_name = "apprunner-g2-mg03"
+    ecr_repo_url = module.ecr_G2MG03.repository_url
+    vpc_id       = module.vpc_G2MG03.vpc_id
+    subnet_ids   = module.vpc_G2MG03.private_subnet
+ }
