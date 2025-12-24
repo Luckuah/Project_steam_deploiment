73c3272d2b00c970f0f43c9238b24710f4bdbf78
Luckuah <lululorang@gmail.com>
2025-12-24 16:08:11 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "s3_bucket_name" {
   description = "Nom du bucket S3 utilisé par Terraform"
-  value       = = var.s3_bucket_name
+  value       =  var.s3_bucket_name
 }
 
 output "ecr_repository_url" {
